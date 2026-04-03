70b6fa26dcfc72cca974571f390610c37f957535
Borovskiy Danil <DBorovskiy@int.businesscar.ru>
2026-04-03 11:22:55 +0300

    Правки по тексту: заменил повторяющиеся элементы в тексте на использование класса OneCWebWMS

--- cli.py
+++ cli.py
@@ -1,19 +1,22 @@
 import os 
 import sys
+
 import typer 
 from typer import colors
 from pywinauto import Application, ElementNotFoundError, ElementAmbiguousError, WindowSpecification
 
+from enums import OneCWebWMS
+
 
 app = typer.Typer(
     name='onec-claim-geely-util',
-    help="""Утилита для заполнения данных рекламации в клиентском приложении 1С WEB VMS (Geely)\n
+    help=f"""Утилита для заполнения данных рекламации в клиентском приложении {OneCWebWMS.APP_NAME}\n
             Требования:
                 • Windows OS
-                • Запущенный экземпляр 1С WEB VMS (Geely)
-                • Открыта вкладка "Рекламация VMS (создание) *"
+                • Запущенный экземпляр {OneCWebWMS.APP_NAME}
+                • Открыта вкладка "{OneCWebWMS.CLAIM_CREATE_TAB_TITLE}"
             
-            С исходным кодом утилиты можно ознакомиться по ссылке - https://github.com/bupbib/onec-util
+            С исходным кодом утилиты можно ознакомиться по ссылке - https://github.com/bupbib/onec-claim-geely-util
     """,
     no_args_is_help=True,
     add_completion=False
@@ -22,13 +25,13 @@ app = typer.Typer(
 
 @app.callback()
 def main(ctx: typer.Context):
-    """
+    f"""
     Callback-функция, выполняемая перед каждой командой.
 
     Вызывает ошибку, если:
         • Операционная система не Windows
-        • Не запущено клиентское приложение 1С WEB VMS (Geely) или запущено более одного окна
-        • В приложении не открыта вкладка "Рекламация VMS (создание)"
+        • Не запущено клиентское приложение {OneCWebWMS.APP_NAME} или запущено более одного окна
+        • В приложении не открыта вкладка "{OneCWebWMS.CLAIM_CREATE_TAB_TITLE}"
     """ 
     if not sys.platform.startswith('win') or os.name != 'nt':
         typer.secho(
@@ -38,14 +41,14 @@ def main(ctx: typer.Context):
         raise typer.Exit(1)
     
     try:
-        geely_app = Application(backend='uia').connect(title='WEB VMS - ООО «Бизнес Кар М» - Москва')
+        geely_app = Application(backend='uia').connect(title=OneCWebWMS.MAIN_WINDOW_TITLE)
         geely_window: WindowSpecification = geely_app.top_window()
 
-        claim_tab = geely_window.child_window(title_re=r'Рекламация VMS \(создание\)\s?\*?', control_type='Pane')
+        claim_tab = geely_window.child_window(title_re=OneCWebWMS.CLAIM_CREATE_TAB_PATTERN, control_type='Pane')
         
         if not claim_tab.exists():
             typer.secho(
-                'Ошибка: Не открыта вкладка "Рекламация VMS (создание)". Откройте вкладку создания рекламации и повторите попытку',
+                f'Ошибка: Не открыта вкладка "{OneCWebWMS.CLAIM_CREATE_TAB_TITLE}". Откройте вкладку создания рекламации и повторите попытку',
                 fg=colors.RED
             )
             raise typer.Exit(1)
@@ -54,13 +57,13 @@ def main(ctx: typer.Context):
         ctx.obj = geely_window
     except ElementNotFoundError as no_app_err:
         typer.secho(
-            'Ошибка: Клиентское приложение 1С WEB VMS (Geely) не запущено. Запустите приложение и повторите попытку',
+            f'Ошибка: Клиентское приложение {OneCWebWMS.APP_NAME} не запущено. Запустите приложение и повторите попытку',
             fg=colors.RED
         )
         raise typer.Exit(1) from no_app_err
     except ElementAmbiguousError as many_window_err:
         typer.secho(
-            'Ошибка: Запущено несколько окон 1С WEB VMS (Geely). Оставьте только одно активное окно и повторите попытку',
+            f'Ошибка: Запущено несколько окон {OneCWebWMS.APP_NAME}. Оставьте только одно активное окно и повторите попытку',
             fg=colors.RED
         )
         raise typer.Exit(1) from many_window_err
@@ -71,8 +74,8 @@ def works(
         ctx: typer.Context,
 ):
     """Тестовая команда"""
-    claim: WindowSpecification = ctx.obj 
-    # claim.print_control_identifiers()
+    window: WindowSpecification = ctx.obj
+    typer.secho("✅ Утилита работает, подключение к Geely установлено", fg=colors.GREEN)
 
 
 if __name__ == '__main__':

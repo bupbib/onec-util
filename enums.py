e7f416c4a35a2d69b4d78fff87602837bc2203c3
Borovskiy Danil <DBorovskiy@int.businesscar.ru>
2026-04-06 23:24:41 +0300

    Добавил класс MyApp

--- enums.py
+++ enums.py
@@ -9,3 +9,12 @@ class OneCWebWMS(StrEnum):
     # Вкладки
     CLAIM_CREATE_TAB_TITLE = 'Рекламация VMS (создание)'
     CLAIM_CREATE_TAB_PATTERN = r'Рекламация VMS \(создание\)\s?\*?'
+
+
+class MyApp(StrEnum):
+    NAME = 'onec-claim-geely-util'
+    ADD_JOBS = 'add-jobs'
+    ADD_DETAILS = 'add-details'
+
+    def __repr__(self) -> str:
+        return f"'{self.value}'"

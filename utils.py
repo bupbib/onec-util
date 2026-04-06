cad43615290b7e403ea638c3bc2cca5993e2a565
Borovskiy Danil <DBorovskiy@int.businesscar.ru>
2026-04-06 13:30:16 +0300

    Создал модуль utils, а в нем реализовал функцию error_exit для централизованного вывода ошибок в коносоль и лог

---
+++ utils.py
@@ -0,0 +1,29 @@
+import logging 
+
+import typer 
+from typer import colors 
+
+
+logger = logging.getLogger(__name__)
+
+
+def error_exit(
+    msg: str, 
+    exit_code: int = 1,
+    original_exception: Exception | None = None
+) -> None:
+    """
+    Выводит сообщение об ошибке красным цветом, логирует и завершает программу с указанным кодом.
+    
+    Args:
+        msg: Сообщение об ошибке для вывода пользователю
+        exit_code: Код выхода (по умолчанию 1)
+        original_exception: Исходное исключение для сохранения traceback (опционально)
+    """ 
+    typer.secho(msg, fg=colors.RED)
+    logger.error(msg)
+    
+    if original_exception is None:
+        raise typer.Exit(exit_code)
+    else:
+        raise typer.Exit(exit_code) from original_exception

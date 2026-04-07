ea07d783e10474e6a001b4b99732343531e95012
Borovskiy Danil <DBorovskiy@int.businesscar.ru>
2026-04-07 21:29:17 +0300

    Добавил еще валидации для полей

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel, Field
 
 class DetailItem(BaseModel):
     model_config = {'extra': 'forbid'}
-    
-    part_number: str = Field(alias='КаталожныйНомер')
-    quantity: float = Field(alias='Количество')
-    upd: str = Field(alias='УПД')
+
+    part_number: str = Field(alias='КаталожныйНомер', min_length=1)
+    quantity: float = Field(alias='Количество', ge=0)
+    upd: str = Field(alias='УПД', min_length=1)
 
